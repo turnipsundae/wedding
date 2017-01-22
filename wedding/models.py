39bd2b18c64bb954235fa606d337b4d96a7182da
Kevin Lau <laudevelopment@gmail.com>
2017-01-22 15:19:42 -0800

    Hook up RSVP to database

--- wedding/models.py
+++ wedding/models.py
@@ -15,9 +15,9 @@ class Meal(models.Model):
 
 # List of guests, their relationship to Kevin or Melissa, whether they are attending, meal choice, comments, and any plus ones. Plus ones are also added to the guest list and are indicated by the added_by field.
 class Guest(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email = models.EmailField()
+    first_name = models.CharField(max_length=100, default="")
+    last_name = models.CharField(max_length=100, default="")
+    email = models.EmailField(default="")
     relationship_to_wedding_party = models.CharField(max_length=200, default="")
     attending = models.NullBooleanField()
     meal = models.ForeignKey(Meal)
--- wedding/views.py
+++ wedding/views.py
@@ -9,7 +9,71 @@ def index(request):
     return render(request, 'wedding/index.html')
 
 def rsvp(request):
+    if request.method == "POST":
+        params = request.POST
+
+        attending = params.get("attendingRadios", None)
+        if attending and attending == "yes":
+            attending = True
+        elif attending and attending == "no":
+            attending = False
+
+        meal = params.get("meal", "chicken")
+        meal = Meal.objects.filter(choice__startswith=meal).get()
+
+        guest = Guest(first_name=params.get("first_name", ""),
+            last_name=params.get("last_name", ""),
+            email=params.get("email", ""),
+            # attending=params.get("attending", ""),
+            attending=attending,
+            meal=meal,
+            relationship_to_wedding_party=params.get("relationshipRadios", ""),
+            comments=params.get("comments", ""),
+            )
+        
+        # print (params.dict())
+
+        guest.save()
+
+        return HttpResponse("Horray Check the Console")
     return render(request, 'wedding/rsvp.html')
+
+# def sign_up(request):
+#   if request.method == "POST":
+#     first_name = request.POST['first_name']
+#     last_name = request.POST['last_name']
+#     username = request.POST['username']
+#     email = request.POST['email']
+#     password = request.POST['password']
+#     error_exists = False
+#     params = dict(first_name=first_name, last_name=last_name, email=email)
+
+#     if not valid_name(first_name) or not valid_name(last_name):
+#       error_exists = True
+#       params['error_name'] = "Enter a first and last name using only letters, apostrophes and hyphens"
+#     if not valid_username(username):
+#       error_exists = True
+#       params['error_username'] = "Enter a username using only letters, numbers, underscore and hyphens"
+#     if not valid_email(email):
+#       error_exists = True
+#       params['error_email'] = "Please enter in the format example@domain.com"
+#     if not valid_password(password):
+#       error_exists = True
+#       params['error_password'] = "Minimum password length is 6 characters"
+#     if error_exists:
+#       return render(request, "workouts/sign_up.html", params)
+
+#     user = User(first_name = first_name,
+#                 last_name = last_name,
+#                 email = email,
+#                 username = username)
+#     user.set_password(password)
+#     user.save()
+#     # TODO add sign up success message to login
+#     return HttpResponseRedirect(reverse("workouts:login"))
+#   else:
+#     return render(request, "workouts/sign_up.html")
+
 
 def registry(request):
     return render(request, 'wedding/registry.html')
